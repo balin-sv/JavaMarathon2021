8ab4f0b13a2c3d935c5d97e55f7c9e20dec225d8
Svetlana Blizniakova <sblizniakova@gmail.com>
2021-01-25 02:32:42 -0300

    Day5/task1_2

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -3,5 +3,24 @@ package day5;
 public class Task1 {
     public static void main(String[] args) {
 
+        Car car1 = new Car();
+
+        createItem(car1);
+        showItem(car1);
+
     }
+
+    public static void createItem(Car a) {
+        a.setColor("green");
+        a.setModel("skoda");
+        a.setYear(2017);
+
+    }
+
+    public static void showItem(Car a) {
+        System.out.println("цвет " + a.getColor());
+        System.out.println("модель " + a.getModel());
+        System.out.println("год выпуска " + a.getYear());
+    }
+
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,15 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike moto1 = new Motorbike(2011, "red", "honda");
+        showItem(moto1);
+
+    }
+
+    public static void showItem(Motorbike a) {
+        System.out.println("год выпуска " + a.getYear());
+        System.out.println("цвет " + a.getColor());
+        System.out.println("модель " + a.getModel());
 
     }
 }
