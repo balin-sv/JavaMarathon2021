1c47886e12840b8ed51f28dd4d39f5369d6f71d7
Svetlana Balin <balinsvet@gmail.com>
2021-02-21 18:52:57 -0300

    Day14

--- src/main/java/day14/Task1.java
+++ src/main/java/day14/Task1.java
@@ -1,7 +1,58 @@
 package day14;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Task1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        File file = new File("test");
+        printSumdigits(file);
 
     }
+
+    public static void printSumdigits(File file) throws IOException {
+        int aux = 0;
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+        } catch (FileNotFoundException ex) {
+            System.out.println("Файл не найден");
+        }
+
+        String line = sc.nextLine();
+
+        sc.close();
+
+        String numbersString[] = line.split(" ");
+
+        if (numbersString.length<10) {
+            try{
+                throw new IOException();
+            } catch (IOException e) {
+                System.out.println("Некорректный входной файл");
+            }
+        }
+
+        int[] numbers = new int[10];
+
+
+        for (int i = 0; i < numbersString.length; i++) {
+            numbers[i] = Integer.parseInt(numbersString[i]);
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            aux = aux + numbers[i];
+
+        }
+
+        System.out.println(Arrays.toString(numbers));
+        System.out.println(aux);
+
+    }
+
 }
+
--- src/main/java/day14/Task3.java
+++ src/main/java/day14/Task3.java
@@ -1,7 +1,64 @@
 package day14;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Task3 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        File file = new File("newfile1");
+
+        System.out.println(parseFileToStringList(file));
+
+    }
+
+    /**
+     *
+     * @param file
+     * @return
+     * @throws IOException
+     */
+    public static List<People> parseFileToStringList(File file) throws IOException {
+
+        List <People> list = new ArrayList<>();
+
+        try {
+            Scanner sc = new Scanner(file);
+
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+
+                String [] aux = line.split(" ");
+
+
+                if (Integer.parseInt(aux[1]) < 0) {
+                    throw new IllegalArgumentException();
+                }
+                People p = new People(aux[0],Integer.parseInt(aux[1]) );
+
+                list.add(p);
+            }
+
+            return list;
+
+        } catch (FileNotFoundException | IllegalArgumentException e) {
+
+            System.out.println("");
+
+        }
+        return null;
 
     }
 }
+
+
+
+
+
+
+
+
